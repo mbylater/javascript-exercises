06968d828507ef87fe4aec8e325d8a4e2c4db2f6
MbyLater <185529385+mbylater@users.noreply.github.com>
2025-03-04 21:40:35 -0500

    solved and tested

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(intFirst , intSecond) {
+    let result = 0;
+    if (!Number.isInteger(intFirst) || !Number.isInteger(intSecond) || intFirst < 0 || intSecond < 0) {
+        return "ERROR"; 
+    } else if (intFirst < intSecond) {
+        for (let i = intFirst; i <= intSecond; i++) {
+            result = result + i;
+        }
+    } else {
+        for (let i = intSecond; i <= intFirst; i++) {
+            result = result + i;
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
