f7706bffbc599bfaef30972b7df6b5842d80b9d7
MbyLater <185529385+mbylater@users.noreply.github.com>
2025-03-04 19:01:23 -0500

    coded and tested

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
-
+// 
+const reverseString = function(string) {
+    // Create an empty string to store the result
+    let reverse = '';
+    // Create variable to store string length
+    let length = string.length;
+    // Loop for from 0 to length (exclusive)
+    for (let i = length - 1; i >= 0; i--) {
+        // Extract last letter and add it to the empty string
+        reverse += string[i];
+    }
+    return reverse
 };
 
 // Do not edit below this line
