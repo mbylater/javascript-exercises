9e148450b502231e8d55fdc9433ac53667875912
MbyLater <185529385+mbylater@users.noreply.github.com>
2025-03-04 20:44:27 -0500

    solved and tested

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(list, ...valuesToRemove) {
+    // Create an empty array to store the result
+    let result = [];
+
+    //Create a loop
+    for (let i = 0; i < list.length; i++) {
+        if (!valuesToRemove.includes(list[i])) {
+            result.push(list[i]);
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
