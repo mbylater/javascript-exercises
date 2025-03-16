ef866887f2c8efd594d6c95940cd21a0ad21817a
MbyLater <185529385+mbylater@users.noreply.github.com>
2025-03-16 17:59:29 -0400

    exercise 8 complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(param1, param2) {
+	const sum = param1 + param2;
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(param3, param4) {
+	const subtractResult = param3 - param4;
+  return subtractResult;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let multiplyResult = 1;
+  for (let i = 0; i < array.length; i++) {
+    multiplyResult = multiplyResult * array[i];
+  }
+  return multiplyResult;
 };
 
-const power = function() {
-	
+const power = function(param5, param6) {
+	const powerResult = Math.pow(param5, param6);
+  return powerResult;
 };
 
-const factorial = function() {
-	
+const factorial = function(param7) {
+  let factorialResult = 1;
+	if (param7 === 0) {
+    return 1
+  } else {
+    for (let i = 1; i <= param7; i++) {
+      factorialResult = factorialResult * i;
+    }
+    return factorialResult;
+  }
 };
 
 // Do not edit below this line
