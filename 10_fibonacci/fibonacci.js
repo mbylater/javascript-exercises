bc2b8b8bbc160b74dfab39bffd36656fdc948f8c
MbyLater <185529385+mbylater@users.noreply.github.com>
2025-03-16 18:42:50 -0400

    exercise 10 completerd

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = Number(n);
+    if (n < 0) {
+        return "OOPS";
+    } else if (n === 0) {
+        return 0;
+    } else if (n === 1) {
+        return 1;
+    } else {
+        return (fibonacci(n - 1) + fibonacci(n - 2)); // Removed 'if' after else
+    }
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
