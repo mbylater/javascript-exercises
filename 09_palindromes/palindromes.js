298ff4b1b450068ef18d03461626f6313474e684
MbyLater <185529385+mbylater@users.noreply.github.com>
2025-03-16 18:29:54 -0400

    complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (inputString) {
+    // Convert inputString to lowercase
+    let lowerCaseInput = inputString.toLowerCase();
+    
+    // Remove all non-alphanumeric characters (like punctuation and spaces)
+    let cleanedStr = lowerCaseInput.replace(/[^a-z0-9]/g, '');
+    
+    // Convert it to array and reverse it
+    let reversedStr = cleanedStr.split('').reverse().join('');
+    
+    // Compare reversed string to the original cleaned string
+    return cleanedStr === reversedStr;
 };
 
 // Do not edit below this line
