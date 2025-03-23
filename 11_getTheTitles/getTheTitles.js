6ba0ccbbf7739c5e2ec1f8e68a7f6b9de01ef8d1
MbyLater <185529385+mbylater@users.noreply.github.com>
2025-03-22 21:06:27 -0400

    complete exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    return books.map(function(book) {
+        return book.title;
+    });
 };
 
 // Do not edit below this line
 module.exports = getTheTitles;
+
