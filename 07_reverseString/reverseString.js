992ff1aa980ec8b228c0f04d6a6a388fcd91901e
Olga Kreft <olgakreft@Olgas-MacBook-Pro.local>
2025-09-16 10:08:29 -0400

    complete task 8

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    //split string into arrey (arr)
+    //create reverseArr
+    //turn reverseArr into string
+    return string.split("").reverse().join("");
 };
 
 // Do not edit below this line
